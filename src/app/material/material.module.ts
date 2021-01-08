d8c68f0c9be19a4048da64f7e16dbb01f2cede93
DESKTOP-FRK722U\Thilina <dilshanthilina53@gmail.com>
2021-01-08 20:38:03 +0530

    initial commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//material
+import {MatButtonModule} from '@angular/material/button'
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    MatButtonModule,
+  ]
+})
+export class MaterialModule { }
